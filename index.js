9435e252bbe25e7114cae03bfeb56a449ab971d9
pat taylor <patrick.schneider9@web.de>
2024-03-10 19:19:27 +0100

    feat: deeper look on if else statements

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// Math built in object
+const subscribe = document.getElementById("myCheckbox")
+const masterCard = document.getElementById("masterCardBtn")
+const paypal = document.getElementById("paypalBtn")
+const submit = document.getElementById("submitBtn")
+const subPara = document.getElementById("subPara")
+const paymentPara = document.getElementById("paymentPara")
 
-const MIN = 50
-const MAX = 120
-
-x = Math.floor(Math.random() * (MAX - MIN)) + MIN;
-
-if (x > 100) {
-    console.log("The number is greater than 100")
+submit.onclick = function() {
+    // ternary operator
+    subPara.textContent = subscribe.checked ? "you are subscribed" : "you are not subscribed yet"
+    if (masterCard.checked) {
+        paymentPara.textContent = "you are using mastercard"
+    }
+    else if (paypal.checked) {
+        paymentPara.textContent = "you are using paypal"
+    }
 }
-else {
-    console.log("Less than 100")
-}
-console.log(x)
+
+
