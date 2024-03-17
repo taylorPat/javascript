d4a83471147db954983ec0bea81b50826077a823
pat taylor <patrick.schneider9@web.de>
2024-03-17 20:26:27 +0100

    feat: dates

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-// sort() = sorting elements of arrays in place in lexicographic order
-const fruits = [
-    {name: "apple", calories: 10},
-    {name: "banana", calories: 33},
-    {name: "birne", calories: 3},
-]
+// Date(year, month, day, hour, minute, second, ms)
+// Date("2024-01-02T12:00:01Z")
+const date = new Date("2023-12-31T23:59:59Z");
+const date2 = new Date("2024-01-01T00:00:00Z")
 
-// sorting objects number
-// fruits.sort((a, b) => b.calories - a.calories)
-// console.log(fruits)
+if (date2 > date) {
+    console.log("Happy New Year!!!")
+}
 
-// sorting objects by alphabet
-fruits.sort((a,b) => a.name.localeCompare(b.name))
-console.log(fruits)
+console.log(date)
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+const hour = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
+const dayOfWeek = date.getDay();
+
+console.log(year, month, day, hour, min, sec, dayOfWeek)
