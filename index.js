30ddc445342c4a4bb92c78ebe2a9e80cae4a9c99
pat taylor <patrick.schneider9@web.de>
2024-03-17 11:15:15 +0100

    feat: nested objects, rest operator

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-// Descrtructuring = extract values from array and objects
+// Nested objects
 
-// 1. Swap two values
-let a = 1;
-let b = 2;
-
-[a, b] = [b, a];
-
-console.log(a, b)
-
-// 2. Swap two values of an array
-const colors = ["red", "blue", "green", "white"];
-
-[colors[0], colors[3]] = [colors[3], colors[0]];
-
-console.log(colors)
-
-// 3. Assign Array values to variables
-const [firstColor, ...secondColors] = colors; // use rest operator ...
-
-console.log(firstColor, secondColors)
-
-// 4. Assign object values to variables
-const person1 = {
-    firstName: "Spongebob",
-    lastName: "Squrepants",
-    age: 20,
-    numberOfHairs: 3
-}
-const {firstName, lastName, job="unemployed", ...numbers} = person1;
-console.log(firstName, lastName, job, numbers.age, numbers.numberOfHairs)
-
-// 5. Destructuring in function parameters
-function displayPerson({firstName, lastName, job="Unemployed"}) {
-    console.log(`Wit function params destructuring: ${firstName}, ${lastName}, ${job}`)
+class Person {
+    constructor(name, age, ...adresse) {
+        this.name = name;
+        this.age = age;
+        console.log(typeof adresse, adresse)
+        this.adresse = new Adresse(...adresse)
+        console.log(typeof this.adresse, this.adresse)
+    }
 }
 
-displayPerson(person1)
+class Adresse {
+    constructor(street, houseNumber, city) {
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.city = city;
+    }
+}
+
+const person = new Person("Patrick", 22, "Baker Street", 29, "Ratisbon")
