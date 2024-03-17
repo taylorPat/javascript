b30fc957348ae7784b021c89d7655045e6843861
pat taylor <patrick.schneider9@web.de>
2024-03-17 11:01:31 +0100

    feat: array and object destructuring

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
+// Descrtructuring = extract values from array and objects
 
-    set width(newWidth) {
-        if (newWidth > 0) {
-            this._width = newWidth;
-        }
-        else {
-            console.error("Width is not valid!")
-        }
-    }
-    
-    set height(newHeight) {
-        if (typeof newHeight === "number" && newHeight > 0) {
-            this._height = newHeight;
-        }
-        else {
-            console.error("Height is not valid!")
-        }
-    }
+// 1. Swap two values
+let a = 1;
+let b = 2;
 
-    get width() {
-        return this._width;
-    }
+[a, b] = [b, a];
 
-    get height() {
-        return this._height;
-    }
+console.log(a, b)
 
-    get area() {
-        return this._height * this._width
-    }
+// 2. Swap two values of an array
+const colors = ["red", "blue", "green", "white"];
+
+[colors[0], colors[3]] = [colors[3], colors[0]];
+
+console.log(colors)
+
+// 3. Assign Array values to variables
+const [firstColor, ...secondColors] = colors; // use rest operator ...
+
+console.log(firstColor, secondColors)
+
+// 4. Assign object values to variables
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "Squrepants",
+    age: 20,
+    numberOfHairs: 3
+}
+const {firstName, lastName, job="unemployed", ...numbers} = person1;
+console.log(firstName, lastName, job, numbers.age, numbers.numberOfHairs)
+
+// 5. Destructuring in function parameters
+function displayPerson({firstName, lastName, job="Unemployed"}) {
+    console.log(`Wit function params destructuring: ${firstName}, ${lastName}, ${job}`)
 }
 
-const rectangle = new Rectangle(100, 200);
-
-console.log(rectangle.width)
-console.log(rectangle.height)
-console.log(rectangle.area)
+displayPerson(person1)
