35f8beb560a295f0cea7813d879eff2371f66bf4
pat taylor <patrick.schneider9@web.de>
2024-03-17 20:50:38 +0100

    feat: closures

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// Date(year, month, day, hour, minute, second, ms)
-// Date("2024-01-02T12:00:01Z")
-const date = new Date("2023-12-31T23:59:59Z");
-const date2 = new Date("2024-01-01T00:00:00Z")
+// closure -> allow for private variables and state maintenance
 
-if (date2 > date) {
-    console.log("Happy New Year!!!")
+function createCounter() {
+    let counter = 0
+
+    function increase() {
+        counter++
+    }
+    
+    function getCounter() {
+        return counter;
+    }
+    
+    return {increase, getCounter}
 }
 
-console.log(date)
+const counts = createCounter();
 
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDay();
-const hour = date.getHours();
-const min = date.getMinutes();
-const sec = date.getSeconds();
-const dayOfWeek = date.getDay();
+counts.increase()
+counts.increase()
+counts.increase()
 
-console.log(year, month, day, hour, min, sec, dayOfWeek)
+console.log(counts.getCounter())
