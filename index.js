7367e97a69f0ddb0bf621f9d75f02ee3235879c9
pat taylor <patrick.schneider9@web.de>
2024-03-24 19:39:31 +0100

    feat: DOM navigation (firstElementChild, lastElementChild, ...)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// element selectors = Methods used to target and manipulate HTML elements.
-// They allow you to select one or multiple HTML elements from the DOM.
+// DOM navigation = The process of navigating through the structure of an HTML
+                // document using JavaScript
 
-// document.getElementbyId() -> Element or Null
-// document.getElementsClassName() -> HTML Collection (
-//    like an HTML element with less functionality. E.g. if you want to use
-//    the forEach methode you first have to Array.from(...).forEach())
-// document.getElementsByTagName() -> HTML Collection
-// document.querySelector() -> First Element or Null -> (".className"), ("h1")
-// document.querySelectorAll() -> Nodelist -> ("li") -> forEach()
+
+
+// .firstElementChild
+// .lastElementChild
+// .nextElementSibling
+// .previousElementSibling
+// .parentElement
+// .children -> document.getElementById("fruits").children[0].style.backgroundColor = "yellow"
