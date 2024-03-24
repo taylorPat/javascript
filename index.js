7c8cf413cf3b2d5cbd8b43da615fac04d60368be
pat taylor <patrick.schneider9@web.de>
2024-03-24 18:53:48 +0100

    feat: Document Object Model (DOM)

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-const display = document.getElementById("display");
-
-function appendtoDisplay(symbol) {
-    console.log("input: ", display.value.at(-1))
-    if (display.value === "" && symbol === ".") {
-        console.log("not possible")
-    }
-    else if (display.value.at(-1) === "." && symbol === ".") {
-        console.log("also not possible")
-    }
-    else {
-        display.value += symbol;
-    }
-    console.log(display.value)
-}
-
-function calculate() {
-    let formula = display.value;
-    try {
-        display.value = eval(formula);
-    }
-    catch(error){
-        display.value = "ERROR"
-    }
-}
-
-function clearDisplay(){
-    display.value = "";
-}
-
-function clearLast() {
-    display.value = display.value.substring(0, display.value.length-1)
-}
+// DOM = Document Object Model
+// It is a object that represent the web page you see in the browser.
+// And provides you with an API to interact with it.
+// Web browser create the DOM when it loads an html document and 
+// structures all the elements in a tree like representation.
+// Javascript can access the DOM to dynamically change the content,
+// structure and style of a web page.
